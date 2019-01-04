6c505ee09bc90feb528fdde808e700da01eed1bb
deepanshut041 <deepanshut041@gmail.com>
2019-01-04 20:11:50 +0530

    Added basic api view now adding urls

---
+++ server/main/views/event.py
@@ -0,0 +1,16 @@
+from rest_framework.viewsets import ModelViewSet, ViewSet
+
+from ..models.event import Event, EventTeam, EventTimeline
+from ..serializers.event import EventSerializer, EventTeamSerializer, EventTimlineSerializer
+
+class EventView(ModelViewSet):
+    serializer_class = EventSerializer
+    queryset = Event.objects.all()
+
+class EventTeamView(ModelViewSet):
+    serializer_class = EventTeamSerializer
+    queryset = EventTeam.objects.all()
+
+class EventTimelineView(ModelViewSet):
+    serializer_class = EventTimelineSerializer
+    queryset = EventTimeline.objects.all()
---
+++ server/main/views/workshop.py
@@ -0,0 +1,25 @@
+from rest_framework.viewsets import ModelViewSet, ViewSet
+
+from ..models.workshop import Workshop, WorkshopFaqs, WorkshopOrganiser, WorkshopPlan, WorkshopProject
+from ..serializers.test import WorkshopSerializer, WorkshopFaqsSerializer, WorkshopOrganiserSerializer, WorkshopPlanSerializer, WorkshopProjectSerializer
+
+
+class WorkshopView(ModelViewSet):
+    serializer_class = WorkshopSerializer
+    queryset = Workshop.objects.all()
+
+class WorkshopFaqsView(ModelViewSet):
+    serializer_class = WorkshopFaqsSerializer
+    queryset = WorkshopFaqs.objects.all()
+
+class WorkshopOrganiserView(ModelViewSet):
+    serializer_class = WorkshopOrganiserSerializer
+    queryset = WorkshopOrganiser.objects.all()
+
+class WorkshopPlanView(ModelViewSet):
+    serializer_class = WorkshopPlanSerializer
+    queryset = WorkshopPlan.objects.all()
+
+class WorkshopProjectView(ModelViewSet):
+    serializer_class = WorkshopProjectSerializer
+    queryset = WorkshopProject.objects.all()
