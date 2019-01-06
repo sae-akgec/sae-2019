84b5b557f675dba9249f0d727c83d4e38e835f38
deepanshut041 <deepanshut041@gmail.com>
2019-01-06 13:44:13 +0530

    Removed old files

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,18 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from "./app.component";
-import { CoreModule } from "./core/core.module";
-import { HomeModule } from "./home/home.module";
-import { AuthModule } from "./auth/auth.module";
 import { AppRoutingModule } from "./app-routing.module";
-import { SharedModule } from "./shared/shared.module";
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, CoreModule, HomeModule, AppRoutingModule, AuthModule, SharedModule
+    BrowserModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
