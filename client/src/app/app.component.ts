480b15ecbbb337252548d536367705efebf7cf3f
deepanshut041 <deepanshut041@gmail.com>
2019-01-09 17:08:19 +0530

    Add Nebular theme to admin now working on it

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { Router, NavigationEnd } from "@angular/router";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'client';
-  constructor(private router:Router){
-  }
-  ngOnInit() {
-    this.router.events.subscribe((evt) => {
-        if (!(evt instanceof NavigationEnd)) {
-            return;
-        }
-        window.scrollTo(0, 0)
-    });
-}
+  title = 'sae';
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MainModule } from "./main/main.module";
-import { SharedModule } from "./shared/shared.module";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
-    BrowserModule, AppRoutingModule, BrowserAnimationsModule, MainModule, SharedModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NbThemeModule.forRoot({ name: 'corporate' }),
+    NbLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
