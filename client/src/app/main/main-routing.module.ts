bb3d80c1ed1ffcfeba45a050712941673b274332
deepanshut041 <deepanshut041@gmail.com>
2019-01-14 23:57:45 +0530

    Added blog design now working on fix errors

--- client/src/app/main/main-routing.module.ts
+++ client/src/app/main/main-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main.component';
 import { LandingComponent } from './landing/landing.component';
-
+import { BlogsComponent } from "./blogs/blogs.component";
+import { BlogListComponent } from './blogs/blog-list/blog-list.component';
+import { BlogDetailComponent } from './blogs/blog-detail/blog-detail.component';
 
 const routes: Routes = [
     {path:'', component:MainComponent, children:[
-        {path:'', component:LandingComponent}
+        {path:'', component:LandingComponent},
+        ,
+      { path: 'blogs', component: BlogsComponent, children:[
+        {path: '', component:BlogListComponent},
+        {path: ':id', component:BlogDetailComponent}]
+      },
     ]}
 ];
 
