f2fbb8b384a13c9af11e880cad66a09fcd587492
Rishabh2324 <rishabh2401jain@gmail.com>
2019-08-20 22:13:59 +0530

    name change

--- client/src/app/main/home/home-team/home-team.component.ts
+++ client/src/app/main/home/home-team/home-team.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-team',
   templateUrl: './home-team.component.html',
-  styleUrls: ['./home-team.component.scss']
+  styleUrls: ['./home-team.component.scss','./imagehover.css',
+               ]
 })
 export class HomeTeamComponent implements OnInit {
 
