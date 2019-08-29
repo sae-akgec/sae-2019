28979ba6efd4778b947a3851ebd77266f0e6a804
Rishabh2324 <rishabh2401jain@gmail.com>
2019-08-30 02:00:57 +0530

    final changes3

--- client/src/app/main/home/home-team/home-team.component.ts
+++ client/src/app/main/home/home-team/home-team.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home-team',
   templateUrl: './home-team.component.html',
-  styleUrls: ['./imagehover.scss']
+  styleUrls: ['./home-team.component.scss']
 })
 export class HomeTeamComponent implements OnInit {
 
