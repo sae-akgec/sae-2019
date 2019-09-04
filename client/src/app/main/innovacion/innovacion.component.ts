9bce6369a7f9c9c10b20c5a19b903eba21e64f80
Rishabh2324 <rishabh2401jain@gmail.com>
2019-09-04 16:59:30 +0530

    newchanges5

--- client/src/app/main/innovacion/innovacion.component.ts
+++ client/src/app/main/innovacion/innovacion.component.ts
@@ -66,21 +66,52 @@ export class InnovacionComponent implements OnInit {
       id: '11',
       images: '/assets/images/19.jpg'
 
-    },
-    {
-      id: '12',
-      images: '/assets/images/14.jpg'
-
     }
   ];
-  currentIndex = 0;
-  speed = 5000;
+  follow = true;
+  enablePan = true;
+
+  index = 8;
+  speed = 3000;
   infinite = true;
   direction = 'right';
   directionToggle = true;
   autoplay = true;
+  avatars = '123456789'.split('').map((x, i) => {
+    const num = i;
+    // const num = Math.floor(Math.random() * 1000);
+    return {
+      url: `/assets/images/${num}.jpg`,
+      id: `${num}.jpg`
+    };
+  });
 
   constructor() { }
+  push() {
+    this.avatars.push(
+      {
+        url: `/assets/images/${this.avatars.length + 1}`,
+        id: `${this.avatars.length + 1}`
+      }
+    );
+  }
+
+  remove() {
+    this.avatars.splice(this.avatars.length - 1, 1);
+  }
+
+
+  indexChanged(index) {
+    console.log(index);
+  }
+
+  toggleDirection($event) {
+    this.direction = this.directionToggle ? 'right' : 'left';
+  }
+
+  a(i) {
+    console.log(i);
+  }
 
   ngOnInit() {
   }
