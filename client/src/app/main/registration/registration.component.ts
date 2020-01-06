a4b60fd651a1d974beaf58df2e265878f0606782
Rishabh2324 <rishabh2401jain@gmail.com>
2020-01-07 02:52:02 +0530

    final fixed

--- client/src/app/main/registration/registration.component.ts
+++ client/src/app/main/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormArray, Validators,FormControl, AbstractControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, Validators,FormControl } from '@angular/forms';
 import { group } from '@angular/animations';
 import { MainService } from '../main.service';
 
@@ -13,7 +13,7 @@ export class RegistrationComponent implements OnInit {
 
   states: Array<String> = ['AR', 'AL', 'CA', 'DC'];
   fruits: Array<String> = ['Mango', 'Grapes', 'Strawberry', 'Oranges'];
-  teamMembers: Array<String> = ['1' ,'2', '3' , '4', '5']
+  teamMembers: ['1','2','3','4','5']
   workshops: Array<String> = ['Innovacion-2020' , 'Aacar-6.0' ]
   branches:Array<String> = ['Computer Science and Engineering',
                             'Information Tecnology and Engineering',
@@ -27,13 +27,17 @@ export class RegistrationComponent implements OnInit {
   nestedForm:FormGroup;
   submission=true;
   button:any;
+  email_pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  phone_pattern = /^[0-9]{10}/                          
   constructor(private _fb: FormBuilder, private _mainService:MainService) { }
 
   ngOnInit() {
     this.button = document.getElementById("submitbutton");
     this.nestedForm = this._fb.group({
       TeamName:[null,[Validators.required, Validators.minLength(2)]],
-      Email:[null,[Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]],
+      Email:[null, Validators.compose([
+        Validators.required, Validators.pattern (this.email_pattern)
+      ])],
       SelectWorkshop:[null,Validators.required],
       //
       members:this._fb.array([this.addmembersgroup()])
@@ -43,10 +47,14 @@ export class RegistrationComponent implements OnInit {
   addmembersgroup(){
     return this._fb.group({
       Name:[null,[Validators.required, Validators.minLength(2)]], 
-      Email:[null,[Validators.required,Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')]],
+      Email:[null, Validators.compose([
+        Validators.required, Validators.pattern (this.email_pattern)
+      ])],
       Branch:[null,Validators.required],
       CollegeName:[null,Validators.required],
-      PhoneNumber:[null,[Validators.required,Validators.pattern('^[0-9]{10}')]],
+      PhoneNumber:[null, Validators.compose([
+        Validators.required, Validators.pattern (this.phone_pattern)
+      ])],
       StudentNumber:[null,Validators.required]  
     })
 
