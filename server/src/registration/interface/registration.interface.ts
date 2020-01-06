9dcd8452fdf8cd743d6ccdf93a464814da1166ac
Rishabh2324 <rishabh2401jain@gmail.com>
2020-01-07 01:38:37 +0530

    Added Registration Form

--- server/src/registration/interface/registration.interface.ts
+++ server/src/registration/interface/registration.interface.ts
@@ -1,7 +1,7 @@
 import { Document } from 'mongoose';
 
 export interface Registration extends Document {
-    readonlyTeamName : string;
+    readonly TeamName : string;
     readonly members : string;
     readonly Name: string;
     readonly Email: string;
--- server/src/registration/schema/registration.schema.ts
+++ server/src/registration/schema/registration.schema.ts
@@ -1,15 +1,14 @@
 import * as mongoose from 'mongoose';
 
 export const RegisterationSchema = new mongoose.Schema({
-    teamName: {type: String , required: true},
-    teamId : {type: String , required: true},
-    teamMember : {type : String , required : true},
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    phone: { type: String, required: true },
-    studentNo : { type : String , required : true},
-    branch: { type: String, required: true },
-    year: { type: String, required: true },
-    collegeName : { type: String, required: true },
+    TeamName: {type: String , required: true},
+    members : {type : String , required : true},
+    Name: { type: String, required: true },
+    Email: { type: String, required: true },
+    PhoneNumber: { type: String, required: true },
+    StudentNumber : { type : String , required : true},
+    Branch: { type: String, required: true },
+    SelectWorkshop: { type: String, required: true },
+    CollegeName : { type: String, required: true },
     created_at: { type: Date, default: Date.now }
 })
