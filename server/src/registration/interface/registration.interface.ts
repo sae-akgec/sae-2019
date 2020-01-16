eebe78df9071c8ac36d07524155e48ab61a69a4b
Rishabh2324 <rishabh2401jain@gmail.com>
2020-01-16 19:00:39 +0530

    ADDED SERVER CHANGES

--- server/src/registration/interface/registration.interface.ts
+++ server/src/registration/interface/registration.interface.ts
@@ -3,13 +3,15 @@ import { Document } from 'mongoose';
 export interface Registration extends Document {
     readonly TeamName : string;
     readonly SelectWorkshop : string; 
-    readonly leaderEmail: string;
     readonly Email: string;
-    readonly members : string;
-    readonly Name: string;
-    readonly PhoneNumber: string;
-    readonly StudentNumber: string;
-    readonly Branch: string;
-    readonly CollegeName : string;
-    readonly created_at: Date;
+    readonly plan : String;
+    readonly teamArray : [{
+        readonly Name: string;
+        readonly PhoneNumber: string;
+        readonly StudentNumber: string;
+        readonly Branch: string;
+        readonly CollegeName : string;
+        readonly created_at: Date;
+    }]
+    
 }
--- server/src/registration/registration.module.ts
+++ server/src/registration/registration.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { HandlebarsAdapter, MailerModule } from '@nest-modules/mailer';
 import { RegistrationController } from './registration.controller';
 import { RegisterationSchema} from './schema/registration.schema';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -7,7 +8,22 @@ import{Registration} from './interface/registration.interface'
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Registration', schema: RegisterationSchema }])
+    MongooseModule.forFeature([{ name: 'Registration', schema: RegisterationSchema }]),
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+        defaults: {
+          from:'"nest-modules" <modules@nestjs.com>',
+        },
+        template: {
+          dir: __dirname + '/templates',
+          adapter: new HandlebarsAdapter(), // or new PugAdapter()
+          options: {
+            strict: true,
+          },
+        },
+      }),
+    }),
   ],
   controllers: [RegistrationController],
   providers: [RegistrationService],
