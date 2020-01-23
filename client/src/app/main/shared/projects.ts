564c702979ab45c4c83cf5617c1668c308060a01
Rishabh2324 <rishabh2401jain@gmail.com>
2020-01-23 08:15:56 +0530

    Added Projects

---
+++ client/src/app/main/shared/projects.ts
@@ -0,0 +1,20 @@
+export class  day1 {
+    name: string;
+    image: string;
+    description: string;
+}
+ export class day2 {
+    name: string;
+    image: string;
+    description: string;
+ }
+ export class day3 {
+    name: string;
+    image: string;
+    description: string;
+ }
+ export class day4 {
+    name: string;
+    image: string;
+    description: string;
+ }
